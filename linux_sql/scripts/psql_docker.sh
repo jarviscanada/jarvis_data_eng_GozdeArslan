ba357265018871529a59fd922a18932fee8ba337
Gozde Arslan <gozdearslan2010@gmail.com>
2021-11-30 02:46:55 +0000

    completed bash script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
-#capture CLI arguments (please do not copy comments)
+
 cmd=$1
 db_username=$2
 db_password=$3
 
 #Start docker
 #Make sure you understand `||` cmd
-sudo systemctl status docker || systemctl ...
+#this is checks if the server running first than starts
+sudo systemctl status docker || systemctl start docker
 
 #check container status (try the following cmds on terminal)
 docker container inspect jrvs-psql
@@ -27,23 +28,41 @@ case $cmd in
   if [ $# -ne 3 ]; then
     echo 'Create requires username and password'
     exit 1
+
   fi
 
   #Create container
-	docker volume ....
-	docker run ....
+	docker volume  create pgdata
+	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
 	exit $?
 	;;
-
+#start
   start|stop)
   #check instance status; exit 1 if container has not been created
-  if [ $container_status ...
-  ...
+  if [ $container_status -ne 2 ]; then
+  echo "Container not created"
+   exit 1
+  else
+    docker container $cmd jrvs-psql
+    docker container start jrvs-psql
+    exit$?
+  fi
+;;
+#stop
+ start|stop)
+  if [ $container_status -ne 2 ]; then
+    echo "Container not created"
+     exit 1
+    else
+      docker container stop jrvs-psql
+      exit$?
+  fi
+;;
 
-  #Start or stop the container
-	docker container $cmd jrvs-psql
-	exit $?
-	;;
+     #Start or stop the container
+
+
+
 
   *)
 	echo 'Illegal command'
