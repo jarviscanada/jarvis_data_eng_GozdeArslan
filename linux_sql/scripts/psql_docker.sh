683a757659d5ed3213c9c2783a80d9b7d1d0f47b
Gozde Arslan <gozdearslan2010@gmail.com>
2021-12-01 19:30:22 +0000

    updated script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -33,10 +33,6 @@ case $cmd in
   #Create container
 
 	docker volume  create pgdata
-
-
-
-
 	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
 	exit $?
 	;;
@@ -67,8 +63,4 @@ case $cmd in
 	exit 1
 	;;
 esac 
-
-
-
-
 
