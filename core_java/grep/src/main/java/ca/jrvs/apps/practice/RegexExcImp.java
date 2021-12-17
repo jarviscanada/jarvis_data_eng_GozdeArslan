9096a6f631fd9331e3f43a210556ca24bf08a155
Gozde Arslan <gozdearslan2010@gmail.com>
2021-12-17 23:20:46 +0000

    updated

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -3,32 +3,57 @@ package ca.jrvs.apps.practice;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/*
+    private static final String IPV4_PATTERN =
+            "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
+
+    private static final Pattern pattern = Pattern.compile(IPV4_PATTERN);
+
+    public static boolean isValid(final String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }*/
+/*Pattern p = Pattern.compile("\n{2,}");
+String line = "\n\n";
+System.out.println(p.matcher(line).find());*/
+
 public class RegexExcImp implements RegexExc {
 
     @Override
     public boolean matchJpeg(String filename) {
         String filePattern= ".*\\.(png|jpg)";
-      //  if (Pattern.compile(filename, Pattern.CASE_INSENSITIVE=filename)){
+      if (Pattern.compile(filename, Pattern.CASE_INSENSITIVE.filename)){
 
-        //    return true;
+           return true;
 
-       // }else{
+       }else{
 
 
             System.out.println("The png|jpg files does not exist");
-        //}
+        }
 
               return false;
     }
 
     @Override
-    public boolean matchIP(String filename) {
-        return false;
+    public boolean matchIP(String ip) {
+
+
+     String ipPattern= "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
+                //"/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/";
+
+
+        boolean b = Pattern.matches("^\\s*$","^\\s*$");
+        return Pattern.compile(ip,Pattern.ipPattern);
     }
 
     @Override
-    public boolean isEmptyLine(String Line) {
-        return false;
+    public boolean isEmptyLine(String line) {
+          String emptyLinePattern ="^\\s*$";
+               boolean b = Pattern.matches("^\\s*$","^\\s*$");
+
+        return b;
+                //Pattern.matches("^\\s*$","^\\s*$");
     }
 
 }
